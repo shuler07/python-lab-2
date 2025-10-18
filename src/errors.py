4045f847fc70cbf52ce2b78ec820b943563fba17
shuler7 <vitalisobolevggg@gmail.com>
2025-10-18 22:23:03 +0300

    Add path correcting in console and log messages

--- src/errors.py
+++ src/errors.py
@@ -2,7 +2,21 @@ from src.logger import logger
 from src.colortext import colorize
 
 
+def clear_path(path: str) -> str:
+    parts = path.replace("\\", "/").split("/")
+    parts = list(part for part in parts if part != ".")
+
+    while ".." in parts:
+        ind = parts.index("..") - 1
+        parts.pop(ind)
+        parts.pop(ind)
+
+    return "\\".join(parts)
+
+
 def path_doesnt_exist_message(path: str) -> None:
+    path = clear_path(path=path)
+
     logger.warning('Path "%s" doesn\'t exist', path)
 
     msg1 = colorize(text="Path", color="red")
@@ -28,14 +42,20 @@ def missing_required_arguments_message(missing_args_str: str) -> None:
 
 
 def path_leads_to_dir_instead_of_file_message(path: str) -> None:
+    path = clear_path(path=path)
+
     logger.error('Directory path received instead of file path: "%s"', path)
+
     msg1 = colorize(text="Directory path received instead of file path:", color="red")
     msg2 = colorize(text=path, color="red", bold=True)
     print(msg1, msg2, sep=" ")
 
 
 def path_leads_to_file_instead_of_dir_message(path: str) -> None:
+    path = clear_path(path=path)
+
     logger.error('File path received instead of directory path: "%s"', path)
+
     msg1 = colorize(text="File path received instead of directory path:", color="red")
     msg2 = colorize(text=path, color="red", bold=True)
     print(msg1, msg2, sep=" ")
